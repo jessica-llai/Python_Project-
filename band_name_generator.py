7bbc2c8df9f016a98a0482fc3fcd1152a4bf068b
Jessica Lai <66378672+jessica-llai@users.noreply.github.com>
2022-06-25 18:27:16 +0100

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,7 @@
+
+print("Hello there, today I will help you to choose a band name.")
+city = input("Which city did you grow up in?")
+pet_name = input("What is the name of your pet?")
+
+print("Your band name is " + city + pet_name)
+
