364ce12e02d16a7bdcff61ee308b17f34b89751a
Jessica Lai <66378672+jessica-llai@users.noreply.github.com>
2022-06-25 20:23:14 +0100

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+total_years = 90 
+years_remain = 90 - int(age)
+months_remain = 12 * years_remain
+weeks_remain = 52 * years_remain
+days_remain = 365 * years_remain
+
+print(f"You have {days_remain} days, {weeks_remain} weeks, {months_remain} months, {years_remain} years left.")
+
+
+
+
+
+
+
+
+
